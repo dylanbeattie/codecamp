087100bf10d9c13faee8d4b6f136e657e8934e5f
Dylan Beattie <dylan@dylanbeattie.net>
2021-09-23 12:45:25 +0100

    Add API discovery endpoint and link from _Layout

---
+++ dotnet/Autobarn.Website/Controllers/api/ApiController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Autobarn.Website.Controllers.api {
+	[Route("api")]
+	[ApiController]
+	public class ApiController : ControllerBase {
+		[HttpGet]
+		[Produces("application/hal+json")]
+		public IActionResult Get() {
+			var json = new {
+				message = "Welcome to the Autobarn API",
+				_links = new {
+					models = new {
+						href = "/api/models"
+					},
+					vehicles = new {
+						href = "/api/vehicles"
+					}
+				},
+				_actions = new {
+					create = new {
+						href = "/api/vehicles",
+						method = "POST",
+						type = "application/json",
+						name = "Create a new vehicle"
+					}
+				}
+			};
+			return Ok(json);
+		}
+	}
+}
